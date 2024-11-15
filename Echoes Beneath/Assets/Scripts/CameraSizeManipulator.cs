f39fc7b6f7f708f8a49900adaefacc3c2c6eda77
unki113d <unkilled135@gmail.com>
2024-11-16 00:16:15 +0200

    Camera added
    + Добавил синемашину
    + Добавил несколько префабов (камеры и канваса)
    + Добавил сцену для теста камеры

---
+++ Echoes Beneath/Assets/Scripts/CameraSizeManipulator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionManipulator : MonoBehaviour
+{
+    [Header("Размер камеры (если пойти налево")]
+    public float leftCameraSize;
+    [Header("Размер камеры (если пойти направо")]
+    public float rightCameraSize;
+    [Header("Размер камеры (если пойти вверх")]
+    public float upCameraSize;
+    [Header("Размер камеры (если пойти вниз")]
+    public float downCameraSize;
+
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.transform.position.x > transform.position.x) //Если пошёл направо
+            {
+                CamController.changeCameraSizeEvent?.Invoke(rightCameraSize);
+            }
+            else if (other.transform.position.x < transform.position.x)//Если пошёл налево
+            {
+                CamController.changeCameraSizeEvent?.Invoke(leftCameraSize);
+            }
+            /*else if (other.transform.position.y > transform.position.y)//Если пошёл вверх
+            {
+                CamController.changeCameraSizeEvent?.Invoke(upCameraSize);
+            }
+            else if (other.transform.position.y < transform.position.y)//Если пошёл вниз
+            {
+                CamController.changeCameraSizeEvent?.Invoke(downCameraSize);
+            }*/
+        }
+    }
+}
