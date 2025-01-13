b88903380d86e64d99dbe9349a5af5ac0faba7e4
unki113d <unkilled135@gmail.com>
2025-01-13 21:27:34 +0200

    Работа с аниматором Бешеного
    + Добавил анимацию перемещения в самой игре
    * Изменил немного размер оверлея темноты на лвл1

--- Echoes Beneath/Assets/Scripts/Enemies/EnemyAI.cs
+++ Echoes Beneath/Assets/Scripts/Enemies/EnemyAI.cs
@@ -15,7 +15,11 @@ public class EnemyAI : MonoBehaviour
     public bool IsPlayerInSight = false; // Check if player is detected
     private float _distanceToPlayer; // Distance from enemy to player
     private AIPath aiPath;
+
+    [Header("Animation")]
+    [SerializeField] private SpriteRenderer _spriteRenderer;
     private Animator _animator;
+    private Vector2 _animationWeight;
 
     [Header("Attacking Player")]
     [SerializeField] private float _attackCooldown; // Attack cooldown
@@ -48,12 +52,13 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         // Проверяем, видит ли враг игрока
-        CheckPlayerVisibility();
+        _animationWeight = CheckPlayerVisibility();
 
         if (IsPlayerInSight)
         {
             // Устанавливаем игрока как цель
             aiPath.destination = _player.position;
+            ChangeAnimationWeight(_animationWeight);
             if (_distanceToPlayer <= _attackRange)
             {
                 if (!_isAttacking)
@@ -64,18 +69,19 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-    private void CheckPlayerVisibility()
+    private Vector2 CheckPlayerVisibility()
     {
         //Debug.Log("CheckPlayerVisibility called"); // Убедимся, что метод вызывается
         _distanceToPlayer = Vector2.Distance(transform.position, _player.position);
         if (_distanceToPlayer > _detectionRange)
         {
-            return;
+            return Vector2.zero;
         }
         // Направление от врага к игроку
         Vector2 direction = (_player.position - transform.position).normalized;
 
 #if UNITY_EDITOR
+        Debug.Log($"angle: {direction}");
         // Визуализация Raycast
         Debug.DrawRay(transform.position, direction * _detectionRange, Color.red);
 #endif
@@ -100,6 +106,7 @@ public class EnemyAI : MonoBehaviour
 #endif
             }
         }
+        return direction;
     }
     IEnumerator AttackPlayer()
     {
@@ -117,6 +124,19 @@ public class EnemyAI : MonoBehaviour
         // Wait for attack cooldown
         yield return new WaitForSeconds(_attackCooldown);
         _isAttacking = false;
+    }
+    private void ChangeAnimationWeight(Vector2 direction)
+    {
+        if (direction.x < 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if (direction.x > 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        _animator.SetFloat("MoveX", direction.x);
+        _animator.SetFloat("MoveY", direction.y);
     }
 #if UNITY_EDITOR
     private void OnDrawGizmos()
---
+++ Echoes Beneath/Assets/Scripts/LightDirection.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDirection : MonoBehaviour
+{
+    [SerializeField] private Transform[] _lightPoints;
+    [SerializeField] private Transform _flashLight;
+
+    [SerializeField] private PlayerAim _playerAim;
+    private Vector2 _playerDirection;
+
+    void Start()
+    {
+        _playerDirection =
+//        _lightPoints[0] = null;
+    }
+
+    void Update()
+    {
+        
+    }
+}
